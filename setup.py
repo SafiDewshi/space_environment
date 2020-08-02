db58e9b9e4501056779c92d4dc8404d6ef370a15
Zephyr <zephyr42@hotmail.co.uk>
2020-08-02 18:19:12 +0100

    added astropy dependency

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 setup(name='gym_orbital_system',
       version='0.0.1',
-      install_requires=['gym', 'numpy', 'poliastro']
+      install_requires=['gym', 'numpy', 'poliastro', 'astropy']
       )
